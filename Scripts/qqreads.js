2d85e0020187a80faeda81d7ed584569465cf33b
WWS321 <849750445@qq.com>
2020-11-30 19:41:46 +0800

    Update qqreads.js

--- Scripts/qqreads.js
+++ Scripts/qqreads.js
@@ -202,7 +202,7 @@ function qqreadinfo() {
       //console.log(data);
       if (logs) $.log(`${jsname}, 用户名: ${data}`);
       info = JSON.parse(data);
-      tz += "\n【用户信息】:" + info.data.user.nickName + "\n";
+      tz += "\n【用户" + (num + 1) + "信息】:" + info.data.user.nickName + "\n";
 
       resolve();
     });
